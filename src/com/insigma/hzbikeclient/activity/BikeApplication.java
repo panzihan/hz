c7f6352213e96e9d55262243d84bfd18d6812c90
潘杰军 <panzihan1993@gmail.com>
2014-06-26 20:44:00 +0800

    Revert "杭州自行车查询V1.0"
    This reverts commit 87fc3e6b6f3821e32371aca5b3ebc002d8ab9801.

--- src/com/insigma/hzbikeclient/activity/BikeApplication.java
+++
@@ -1,9 +0,0 @@
-package com.insigma.hzbikeclient.activity;
-
-import android.app.Application;
-
-public class BikeApplication extends Application {
-
-	
-	
-}
--- src/com/insigma/hzbikeclient/util/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.insigma.hzbikeclient.util;
-
-public class Constants {
-	public static final String DATABASE_NAME = "hzbike.sqlite";
-	
-	public static final String CATCH_URL = "http://hangzhou.busditu.com/bike_stopdetails.aspx?screenwidth=240&token=&ua=&area=440100&version=3&backurl=&columnid=9662&mobile=";
-}
